327bd2ab44305954b82efe85894a76a66bfc9801
ilepilov@urchinsys.com <ilepilov@urchinsys.com>
2020-02-26 14:55:20 +0200

    Change Cloudmersive Api Key

--- src/main/java/com/urchinsys/imagetranslator/config/BeanConfig.java
+++ src/main/java/com/urchinsys/imagetranslator/config/BeanConfig.java
@@ -4,6 +4,7 @@ import com.cloudmersive.client.invoker.ApiClient;
 import com.cloudmersive.client.invoker.auth.ApiKeyAuth;
 import com.urchinsys.imagetranslator.converter.WordsApiResponseToWordDefinitionDto;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -15,7 +16,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class BeanConfig {
-  public static final String CLOUD_MERSIVE_API_KEY = "0f49f764-a860-4ba9-8e9f-6c3cb79fa30b";
+  @Value("${cloud_mersive_api_key}")
+  public String CLOUD_MERSIVE_API_KEY;
 
   @Bean
   public Docket api() {
